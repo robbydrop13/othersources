236f0765e105a3f2559aca57437e7358859bd75e
Robin Bonduelle <robin.bonduelle@gmail.com>
2017-06-06 21:58:06 +0200

    add main-lbc and main-pap

---
+++ main-pap.js
@@ -0,0 +1,14 @@
+const helpers = require('./helpers.js');
+const setup = require('./config.js').setup();
+const async = require('async');
+
+console.log("Starting PAP");
+helpers.getNewApparts('pap', setup.pap);
+
+// console.log("Starting Leboncoin");
+// helpers.getNewApparts('leboncoin', setup.leboncoin);
+
+// setTimeout(function() {
+//   console.log("Starting PAP");
+//   helpers.getNewApparts('pap', setup.pap);
+// }, 50 * 1000);
